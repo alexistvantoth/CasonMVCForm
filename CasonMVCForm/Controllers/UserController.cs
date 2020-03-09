80edc7f7dedd4ab0bd6e0bb1a682378d9dc78bf0
enigma <enigma931104@gmail.com>
2020-03-10 00:15:26 +0100

    Add project files.

---
+++ CasonMVCForm/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CasonMVCForm.Models;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace CasonMVCForm.Controllers
+{
+    public class UserController : Controller
+    {
+        public static List<User> list = new List<User>();
+        bool dataValid;
+        bool emailIsValid;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            emailIsValid = new EmailAddressAttribute().IsValid(user.Email);
+            dataValid = isDataValid(user);
+            if ( list.Count == 0 && dataValid && emailIsValid && user.Email.Length < 100)
+            {
+                list.Add(user);
+                ViewData["Message"] = "User " + user.Name + " successfully created";
+            }
+            else {
+                if (!list.Any(x => x.Name.Equals(user.Name)) && user.Name.Length < 64
+                    && !list.Any(x => x.Email.Equals(user.Email)) && emailIsValid && user.Email.Length < 100)
+                {
+                    list.Add(user);
+                    ViewData["Message"] = "User " + user.Name + " successfully created";
+                }
+                else
+                {
+                    if (list.Any(x => x.Name.Equals(user.Name)))
+                    {
+                        ViewData["Message"] = "User " + user.Name + " already exists";
+                    }
+                    if (user.Name.Length >= 64)
+                    {
+                        ViewData["Message"] = "Username too long. Maximum size: 64";
+                    }
+                    if (list.Any(x => x.Email.Equals(user.Email)))
+                    {
+                        ViewData["Message"] = "User with " + user.Email + " email address already exists";
+                    }
+                    if (!emailIsValid)
+                    {
+                        ViewData["Message"] = "Email " + user.Email + " is not in the correct format";
+                    }
+                    if (user.Email.Length >= 100)
+                    {
+                        ViewData["Message"] = "Email address too long: Maximum size: 100";
+                    }
+                }
+            }
+
+            ViewBag.Users = list;
+            return View(user);
+        }
+        
+        public bool isDataValid(User user)
+        {
+            if (user.Name.Length >= 64)
+            {
+                ViewData["Message"] = "Username too long. Maximum size: 64";
+                return false;
+            }
+            else if (user.Email.Length >= 100)
+            {
+                ViewData["Message"] = "Email address too long: Maximum size: 100";
+                return false;
+            }
+            else if (!emailIsValid)
+            {
+                ViewData["Message"] = "Email " + user.Email + " is not in the correct format";
+                return false;
+            }
+            else return true;
+        }
+    }
+}
---
+++ CasonMVCForm/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CasonMVCForm.Models
+{
+    public class User
+    {
+        [Key]
+        [Required(ErrorMessage = "Please enter username")]
+        [StringLength(64, ErrorMessage = "Name too long. Max chars: 64")]
+        [Display(Name= "User Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(100, ErrorMessage = "Email address too long. Max chars: 100")]
+        [EmailAddress(ErrorMessage ="Date is not in correct format (format: xxx@yyy.zz")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Select your gender")]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please provide your birth date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "yyyy.MM.dd")]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+        public String Comment { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + this.Name + ", Email: " + this.Email + ", Gender: " + this.Gender + ", Birthdate: " + this.BirthDate.Year
+                + "." + this.BirthDate.Month + "." + this.BirthDate.Day + ", Comment: " + this.Comment;
+        }
+    }
+}
